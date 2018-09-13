7539ad536fab40be32a804c05b157fc081a9e6dd
Hengyu Lou <hylou34@gmail.com>
2018-09-12 22:26:11 -0700

    Update about page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -13,23 +13,21 @@ class About extends Component {
           </div>
           <div className="nine columns main-col">
             <h2>About Me</h2>
-            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-              eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-              voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-              voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-              Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.
+            <p>I am Hengyu Lou, a master student in the department of Electrical & Computer Engineering at UCLA. 
+            I am seeking 2019 new grad Software Engineer opportunities. I will graduate in March 2019. 
+            I have experience in both the backend such as Ruby on Rails, PHP, NodeJS/Express and frontend such as React/Redux and Bootstrap as well as RDBMS and NoSQL such as MySQL, PostgreSQL and MongoDB. 
+            Also, I have done projects in machine learning and big data processing, for example, spark, numpy and scikit-learn. 
             </p>
             <div className="row">
               <div className="columns contact-details">
-                <h2>Contact Details</h2>
+                <h2>Contact Info</h2>
                 <p className="address">
-                  <span>Jonathan Doe</span><br />
-                  <span>1600 Amphitheatre Parkway<br />
-                    Mountain View, CA 94043 US
+                  <span>Hengyu Lou</span><br />
+                  <span>825 Weyburn Terrace<br />
+                    Los Angeles, CA 90024, US
                   </span><br />
-                  <span>(123)456-7890</span><br />
-                  <span>anyone@website.com</span>
+                  <span>(408) 306-9383</span><br />
+                  <a href="mailto:hylou34@gmail.com"><span>hylou34@gmail.com</span></a>
                 </p>
               </div>
               <div className="columns download">
