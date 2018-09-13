7f7a28fb7b3c8d70862edc60a91a6becb83d5368
Hengyu Lou <hylou34@gmail.com>
2018-09-12 20:48:05 -0700

    Finish home page

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -21,19 +21,23 @@ class Header extends Component {
         </nav> {/* end #nav-wrap */}
         <div className="row banner">
           <div className="banner-text">
-            <h1 className="responsive-headline">I'm Jonathan Doe.</h1>
-            <h3>I'm a Manila based <span>graphic designer</span>, <span>illustrator</span> and <span>webdesigner</span> creating awesome and
-              effective visual identities for companies of all sizes around the globe. Let's <a className="smoothscroll" href="#about">start scrolling</a>
+            <h1 className="responsive-headline">I'm Hengyu Lou.</h1>
+            <h3>I am a master student in the department of <span>Electrical & Computer Engineering</span> at <span>UCLA</span>.
+            I am seeking 2019 new grad <span>Software Engineer</span> opportunities. 
+            <a className="smoothscroll" href="#about"> Start scrolling </a>
               and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
-            <hr />
+            {/*<hr />*/}
             <ul className="social">
-              <li><a href="#"><i className="fa fa-facebook" /></a></li>
+              <li><a href="mailto:hylou34@gmail.com"><i className="fa fa-envelope" /></a></li>
+              <li><a href="https://www.linkedin.com/in/hengyulou/"><i className="fa fa-linkedin" /></a></li>
+              <li><a href="https://github.com/henrylou"><i className="fa fa-github" /></a></li>
+              <li><a href="https://www.facebook.com/profile.php?id=100009805601098"><i className="fa fa-facebook" /></a></li>
+              {/*
               <li><a href="#"><i className="fa fa-twitter" /></a></li>
               <li><a href="#"><i className="fa fa-google-plus" /></a></li>
-              <li><a href="#"><i className="fa fa-linkedin" /></a></li>
               <li><a href="#"><i className="fa fa-instagram" /></a></li>
-              <li><a href="#"><i className="fa fa-dribbble" /></a></li>
               <li><a href="#"><i className="fa fa-skype" /></a></li>
+              */}
             </ul>
           </div>
         </div>
